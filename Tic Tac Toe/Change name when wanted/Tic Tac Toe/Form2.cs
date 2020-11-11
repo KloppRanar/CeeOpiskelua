2efe9a40cba46ec4cba027ebb74e81f3f6f3de1f
KloppRanar <59951688+KloppRanar@users.noreply.github.com>
2020-11-11 11:22:14 +0200

    Add files via upload

---
+++ Tic Tac Toe/Change name when wanted/Tic Tac Toe/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Code for player names
+        {
+            //Form1.setPlayerNames(p1.Text, p2.Text);
+            this.Close();
+        }
+
+        private void p2_KeyPress(object sender, KeyPressEventArgs e)  //This code idea is to use keyboard only (TAB, Enter)
+        {
+            if (e.KeyChar.ToString() == "\n")
+                button1.PerformClick();
+        }
+    }
+}
